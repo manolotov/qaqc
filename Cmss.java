f431e44e99e577b4ce526c61081f7a42d09b3c10
manolotov <142637474+manolotov@users.noreply.github.com>
2023-08-19 20:22:33 -0500

    primer commit
    asd

---
+++ Cmss.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cmss;
+
+/**
+ *
+ * @author icono
+ */
+public class Cmss {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
